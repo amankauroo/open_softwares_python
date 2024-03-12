b6f08a1f5fdfeb21848f046240773ffe03114a82
Aman Kauroo <63089958+amankauroo@users.noreply.github.com>
2024-03-12 19:18:18 +0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from AppOpener import open
+
+#open("microsoft office word")
+#open("phone link")
+#open("settings")
+#open("discord")
+#open("google chrome")
+#open("unity hub")
+#open("file explorer")
+#open("visual studio code")
+#open("whatsapp")
+#open("youtube")
+#open("facebook")
+#open("instagram")
+#open("messenger")
+#open("tiktok")
+#open("twitter")
+#open("notepad")
+#open("control panel")
+#open("task manager")
+#open("disk cleanup")
+#open("defragment and optimize drives")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
